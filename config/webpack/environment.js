d6eaa18a0070136ca5b8683854e7371da266defe
ayanohi <pllayam@yahoo.co.jp>
2021-02-14 23:14:32 +0900

    bootstrapの再導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// jQuery を直接使用する場合は以下を追加する
+// const webpack = require('webpack')
+// environment.plugins.append(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery',
+//     Popper: ['popper.js', 'default']
+//   })
+// )
