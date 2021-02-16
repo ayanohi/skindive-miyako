4efce2b832693ad8eed81d257c40d1a4cb601e04
ayanohi <pllayam@yahoo.co.jp>
2021-02-16 19:32:35 +0900

    feat: seedsファイルにareas初期データ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+areas = Area.create([
+  {name: '宮古島 - 東海岸'},
+  {name: '宮古島 - 南海岸'},
+  {name: '宮古島 - 西海岸'},
+  {name: '伊良部島'},
+  {name: '下地島'},
+  {name: '八重干瀬'},
+  {name: '来間島'},
+  {name: '池間島'},
+  {name: '大神島'},
+  {name: '多良間島'},
+  {name: '水納島'}
+])
