8a1ed0ccaf4f3575b56741e96291727ac37888e8
ayanohi <pllayam@yahoo.co.jp>
2021-02-21 19:41:47 +0900

    feat: 中間テーブルspot_creaturesを経由してspotsとcreaturesを関連付け

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,2 +1,4 @@
 class Creature < ApplicationRecord
+  has_many :spot_creatures
+  has_many :spots, through: :spot_creatures
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :area
+  has_many :spot_creatures
+  has_many :creatures, through: :spot_creatures
   mount_uploader :image, ImageUploader
 
   with_options presence: true do
