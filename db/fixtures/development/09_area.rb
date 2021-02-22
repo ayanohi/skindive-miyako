34b518320ce76e64d68b2122809bd8c05070f559
ayanohi <pllayam@yahoo.co.jp>
2021-02-22 14:16:58 +0900

    perf: 初期データseedをseed_fuのファイルへ統一

--- db/fixtures/development/09_area.rb
+++ db/fixtures/development/09_area.rb
@@ -4,10 +4,10 @@ Area.seed(:id,
   { id: 3, name: '宮古島 - 西' },
   { id: 4, name: '伊良部島' },
   { id: 5, name: '下地島' },
-  { id: 6, name: '八重干瀬' }
-  { id: 7, name: '来間島' }
-  { id: 8, name: '池間島' }
-  { id: 9, name: '大神島' }
-  { id: 10, name: '多良間島' }
+  { id: 6, name: '八重干瀬' },
+  { id: 7, name: '来間島' },
+  { id: 8, name: '池間島' },
+  { id: 9, name: '大神島' },
+  { id: 10, name: '多良間島' },
   { id: 11, name: '水納島' }
 )
