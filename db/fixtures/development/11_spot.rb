d0203368bccfc8e9704951c04696cee5ebee575e
ayanohi <pllayam@yahoo.co.jp>
2021-02-22 21:44:18 +0900

    feat: spotモデルにseed-fuで初期データ投入

---
+++ db/fixtures/development/11_spot.rb
@@ -0,0 +1,80 @@
+Spot.seed do |s|
+  s.id = 1
+  s.name = "白鳥岬"
+  s.description = '好条件が揃ったら絶対に行きたいところ。時季と運が良ければマンタに会えると有名です。白鳥岬公園に車を泊めて、岩場を５分ほど歩いてエントリーします。
+  上級者向けの洞窟もあり、流れがなければ本当に神秘的なスポットです。条件が悪ければ車で通りかかっても分かるはず、大きな白波が見えるので、別スポットへ行きましょう。'
+  s.postal_code = '906-0000'
+  s.address = '沖縄県宮古島市'
+  s.image = Rails.root.join("db/fixtures/img/白鳥岬.jpg").open
+  s.area_id = 4
+end
+
+Spot.seed do |s|
+  s.id = 2
+  s.name = 'サバウツガー'
+  s.description = '行きたい
+  行きたい行きたい
+  行きたい行きたい行きたい'
+  s.postal_code = '906-0000'
+  s.address = '沖縄県宮古島市'
+  s.image = Rails.root.join("db/fixtures/img/サバウツガー.jpg").open
+  s.area_id = 4
+end
+
+Spot.seed do |s|
+  s.id = 3
+  s.name = '与那覇前浜'
+  s.description = '行きたい
+  行きたい行きたい
+  行きたい行きたい行きたい'
+  s.postal_code = '906-0000'
+  s.address = '沖縄県宮古島市'
+  s.image = Rails.root.join("db/fixtures/img/与那覇前浜.jpg").open
+  s.area_id = 3
+end
+
+Spot.seed do |s|
+  s.id = 4
+  s.name = '17END'
+  s.description = '行きたい
+  行きたい行きたい
+  行きたい行きたい行きたい'
+  s.postal_code = '906-0000'
+  s.address = '沖縄県宮古島市'
+  s.image = Rails.root.join("db/fixtures/img/17END.jpg").open
+  s.area_id = 5
+end
+
+Spot.seed do |s|
+  s.id = 5
+  s.name = 'シギラ'
+  s.description = '行きたい
+  行きたい行きたい
+  行きたい行きたい行きたい'
+  s.postal_code = '906-0000'
+  s.address = '沖縄県宮古島市'
+  s.image = Rails.root.join("db/fixtures/img/シギラ.jpg").open
+  s.area_id = 2
+end
+
+Spot.seed do |s|
+  s.id = 6
+  s.name = 'イフ'
+  s.description = '行きたい
+  行きたい行きたい
+  行きたい行きたい行きたい'
+  s.postal_code = '906-0000'
+  s.address = '沖縄県宮古島市'
+  s.image = Rails.root.join("db/fixtures/img/イフ.jpg").open
+  s.area_id = 6
+end
+
+# Spot.seed do |s|
+#   s.id =
+#   s.name = ''
+#   s.description = ''
+#   s.postal_code = '906-0000'
+#   s.address = '沖縄県宮古島市'
+#   s.image = Rails.root.join("db/fixtures/img/1.jpg").open
+#   s.area_id =
+# end
