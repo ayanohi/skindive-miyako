c4d0ee4a47891ce1910c3cf4b8fad45322641046
ayanohi <pllayam@yahoo.co.jp>
2021-02-23 15:46:58 +0900

    delete: spot_creature中間テーブルの初期データを一旦コメントアウト

--- db/fixtures/development/12_spot_creature.rb
+++ db/fixtures/development/12_spot_creature.rb
@@ -1,5 +1,5 @@
-SpotCreature.seed do |s|
-  s.id = 1
-  s.spot = Spot.find_by(name: '白鳥岬')
-  s.creature = Creature.find_by(name: 'ウミガメ')
-end
+# SpotCreature.seed do |s|
+#   s.id = 1
+#   s.spot = Spot.find_by(name: '白鳥岬')
+#   s.creature = Creature.find_by(name: 'ウミガメ')
+# end
