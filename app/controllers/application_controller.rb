7536036ac2752ba2fbd1cafdc00c3f252d24a97d
ayanohi <pllayam@yahoo.co.jp>
2021-02-25 19:00:06 +0900

    feat: 新規登録とアカウント編集時に保存可能となるカラムを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name image introduction pb sns admin))
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name admin))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name image introduction pb sns))
   end
 end
