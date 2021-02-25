cba61c99ccad86f2b0396c3ca6936fc9090bf92e
ayanohi <pllayam@yahoo.co.jp>
2021-02-25 18:35:15 +0900

    feat: usersテーブルにカラムを追加してマイグレーション実行

--- db/migrate/20210225091131_add_column_to_user.rb
+++ db/migrate/20210225091131_add_column_to_user.rb
@@ -1,4 +1,10 @@
 class AddColumnToUser < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :name, :string, null: false, default: '', limit: 30
+    add_column :users, :image, :string
+    add_column :users, :introduction, :text, limit: 500
+    add_column :users, :pb, :string, limit: 5
+    add_column :users, :sns, :string, limit: 50
+    add_column :users, :admin, :integer, null: false, default: 0
   end
 end
