fffb8f3d58436fb3386b44e7def619e345d9dca5
ayanohi <pllayam@yahoo.co.jp>
2021-02-26 21:11:30 +0900

    fix: InvalidAuthenticityTokenエラーに対して、createアクションのみCSRFトークンの検証を無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :get_features
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, only: :create
 
   def get_spots
     @spots = Spot.all
