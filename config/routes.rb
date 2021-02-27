249383e0e97211178840fe835c85596560142866
ayanohi <pllayam@yahoo.co.jp>
2021-02-27 17:34:14 +0900

    feat: commentsルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   root "tops#index"
-  resources :spots
+  resources :spots do
+    resources :comments
+  end
   resources :users, only: :show
   resources :areas, only: :index
 end
