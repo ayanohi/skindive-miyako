349050a8b4e1a6bd5e646a8d1a0a71784a1e016f
ayanohi <pllayam@yahoo.co.jp>
2021-02-27 13:17:09 +0900

    fix: userモデルマイグレーションファイル修正。adminをbooleanへ修正、nameとsnsに一意性制約追加、ファイルを一つにまとめる

--- db/migrate/20210225075059_devise_create_users.rb
+++ db/migrate/20210225075059_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name, limit: 30,    null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :image
+      t.text :introduction, limit: 500
+      t.string :pb, limit: 3
+      t.string :sns, limit: 50
+      t.boolean :admin,                          default: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,7 +42,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
+    add_index :users, :name,                 unique: true
     add_index :users, :email,                unique: true
+    add_index :users, :sns,                  unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
