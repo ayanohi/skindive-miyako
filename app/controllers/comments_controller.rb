1e4a6512117e4ada15304d64f3bae2f6b3312cf7
ayanohi <pllayam@yahoo.co.jp>
2021-03-01 21:17:59 +0900

    feat: 非ログイン時でもコメントが見られるよう、before_actionにexceptを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @spot = Spot.find_by(id: params[:spot_id])
