68bbd0628724fea20dc6b1a186e43c9d8c10a40d
ayanohi <pllayam@yahoo.co.jp>
2021-03-02 23:53:58 +0900

    feat: jQuery を直接使用できるよう webpack ファイル追記

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,12 +3,12 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 
 // jQuery を直接使用する場合は以下を追加する
-// const webpack = require('webpack')
-// environment.plugins.append(
-//   'Provide',
-//   new webpack.ProvidePlugin({
-//     $: 'jquery/src/jquery',
-//     jQuery: 'jquery/src/jquery',
-//     Popper: ['popper.js', 'default']
-//   })
-// )
+const webpack = require('webpack')
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
