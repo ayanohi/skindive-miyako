e96d3d533e56cc79c44d04208ac2b32da46ce233
ayanohi <pllayam@yahoo.co.jp>
2021-03-02 15:54:27 +0900

    feat: history機能をログイン中のユーザーにのみ許可

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,6 @@
 class HistoriesController < ApplicationController
   before_action :set_spot
+  before_action :authenticate_user!
 
   def create
     @history = History.create(user_id: current_user.id, spot_id: @spot.id)
