205d6c3436f034c6e38660bd3fd75c979fb4b5a3
ayanohi <pllayam@yahoo.co.jp>
2021-03-11 15:39:32 +0900

    fix: 非ログイン時もコメント欄のリンクからユーザページ閲覧可能となるよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @user = current_user
 
-    histories = History.where(user_id: current_user.id).order(created_at: :desc).pluck(:spot_id)
+    histories = History.where(user_id: @user.id).order(created_at: :desc).pluck(:spot_id)
     @history_list = Spot.find(histories)
-    clips = Clip.where(user_id: current_user.id).order(created_at: :desc).pluck(:spot_id)
+    clips = Clip.where(user_id: @user.id).order(created_at: :desc).pluck(:spot_id)
     @clip_list = Spot.find(clips)
   end
 end
