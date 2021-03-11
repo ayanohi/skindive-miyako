06cf3087e7a207f015b2102d008bbef174c38a49
ayanohi <pllayam@yahoo.co.jp>
2021-03-11 18:50:17 +0900

    refactor: commentsコントローラの重複処理を before_action としてまとめて記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,17 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: %i[index]
+  before_action :set_comment, only: %i[index new create edit update]
 
   def index
-    @spot = Spot.find_by(id: params[:spot_id])
     @comments = Comment.where(spot_id: params[:spot_id])
   end
 
   def new
-    @spot = Spot.find_by(id: params[:spot_id])
     @comment = Comment.new
     @info = "口コミを書く"
   end
 
   def create
-    @spot = Spot.find_by(id: params[:spot_id])
     @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to spot_comments_path
@@ -23,13 +21,11 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @spot = Spot.find_by(id: params[:spot_id])
     @comment = Comment.find(params[:id])
     @info = "口コミを編集する"
   end
 
   def update
-    @spot = Spot.find_by(id: params[:spot_id])
     @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
       redirect_to spot_comments_path
@@ -48,6 +44,10 @@ class CommentsController < ApplicationController
   end
 
   private
+  def set_comment
+    @spot = Spot.find_by(id: params[:spot_id])
+  end
+
   def comment_params
     params.require(:comment).permit(
       :content,
