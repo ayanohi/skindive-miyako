d8d22df3c3b8a008c790c490d85eecffcbc24691
ayanohi <pllayam@yahoo.co.jp>
2021-03-16 22:37:15 +0900

    fix: config/deploy.rb ファイルで set :rbenv_ruby のバージョン指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.16.0"
 
 set :application, "miyako"
 set :repo_url, "git@github.com:ayanohi/skindive-miyako.git"
-set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_ruby, '2.7.2'
 
 # Nginxの設定ファイル名と置き場所を修正
 set :nginx_config_name, "#{fetch(:application)}.conf"
