cec392f5729786a1177845632e263ee64f035b33
ayanohi <pllayam@yahoo.co.jp>
2021-03-16 20:22:07 +0900

    chore: デプロイの設定ファイルを修正・追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,19 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "miyako"
+set :repo_url, "git@github.com:ayanohi/skindive-miyako.git"
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+# Nginxの設定ファイル名と置き場所を修正
+set :nginx_config_name, "#{fetch(:application)}.conf"
+set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
+
+# 共有ファイルの設定
+append :linked_files, "config/master.key"
+
+# 共有ディレクトリの設定
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
