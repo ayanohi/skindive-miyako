f0ce86edebdf2bf5398cec0ac813367c38199e82
ayanohi <pllayam@yahoo.co.jp>
2021-05-05 16:43:43 +0900

    chore: railsバージョンアップ後rails app:updateタスクを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_081935) do
+ActiveRecord::Schema.define(version: 2021_05_05_065959) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_081935) do
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "latitude", precision: 16, scale: 14
     t.decimal "longitude", precision: 17, scale: 14
+    t.integer "comments_count", default: 0, null: false
     t.index ["area_id"], name: "index_spots_on_area_id"
   end
 
